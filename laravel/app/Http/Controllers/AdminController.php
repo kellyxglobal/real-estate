6bab1204a13d3cba8ba3d9e6cedbb4c4a865c3b4
kellyxglobal <kellyxglobal@gmail.com>
2024-04-07 08:55:45 +0100

    Worked on Admin Logout Option that redirects admin to default login page

--- laravel/app/Http/Controllers/AdminController.php
+++ laravel/app/Http/Controllers/AdminController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Auth;
 
 use Illuminate\Http\Request;
 
@@ -9,4 +10,14 @@ class AdminController extends Controller
     public function AdminDashboard(){
         return view('admin.index');
     } //End Method
+
+    public function AdminLogout(Request $request){
+            Auth::guard('web')->logout();
+
+            $request->session()->invalidate();
+
+            $request->session()->regenerateToken();
+
+            return redirect('/login');
+        }
 }
