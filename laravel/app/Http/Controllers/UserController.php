b4f2b95d3f30b0c325e34fc7a36c58d2621d20f4
kellyxglobal <kellyxglobal@gmail.com>
2024-04-12 17:54:38 +0100

    Completed loading all the frontend components needed

--- laravel/app/Http/Controllers/UserController.php
+++ laravel/app/Http/Controllers/UserController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class UserController extends Controller
 {
     public function Index(){
-        return view('frontend.frontend_dashboard');
+        return view('frontend.index');
     } // End Method 
 }
