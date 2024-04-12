eda4563a8aa2cfcb75b6519c086eb386afe608c3
kellyxglobal <kellyxglobal@gmail.com>
2024-04-12 09:43:23 +0100

    Working on the admin change password

--- laravel/app/Http/Controllers/AdminController.php
+++ laravel/app/Http/Controllers/AdminController.php
@@ -64,5 +64,15 @@ class AdminController extends Controller
 
         return redirect()->back()->with($notification);
 
-     }// End Method 
+     }// End Method
+     
+     
+     public function AdminChangePassword(){
+
+        $id = Auth::user()->id;
+       $profileData = User::find($id);
+       return view('admin.admin_change_password',compact('profileData'));
+
+    }// End Method
+    
 }
